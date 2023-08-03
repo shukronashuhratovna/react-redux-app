e9c00c4971357171c29e71df8b808851b0c86a53
Shuhrat <shuhrat95eshqulov@gmail.com>
2023-08-04 00:55:29 +0500

    login & register add

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useState } from 'react'
+import { icon } from '../constants'
+import { Input } from '../ui'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
-        <div>Login</div>
+        <div className='form-sign-in m-auto w-25 text-center'>
+            <form>
+                <img className="mb-4" src={icon} alt="" width="72" height="60" />
+                <h1 className="h3 mb-3 fw-normal">Please login</h1>
+
+                <Input type='email' label={'Email'} state={email} setState={setEmail} />
+                <Input type='password' label={'Password'} state={password} setState={setPassword} />
+
+                <button className="btn btn-primary w-100 py-2 mt-2" type="submit">Sign In</button>
+            </form>
+        </div>
     )
 }
-
 export default Login
